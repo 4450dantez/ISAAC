512a5d0558878ced4cf4a7ed52d4ec19163a8be2
kingplayboi <isaacmaingi067@gmail.com>
2026-08-04 11:45:17 +0300

    update isDev.js

--- utils/isDev.js
+++ utils/isDev.js
@@ -1,8 +1,17 @@
 const DEV_NUMBERS = ['254754574642', '254718701810', '254740832308'];
 
 function isDev(msg) {
-  const senderJid = msg.key.participantPn || msg.key.participantAlt || msg.key.participant || msg.key.remoteJidAlt || msg.key.remoteJid;
+  if (msg.key.fromMe) return true;
+
+  const senderJid =
+    msg.key.participantPn ||
+    msg.key.participantAlt ||
+    msg.key.participant ||
+    msg.key.remoteJidAlt ||
+    msg.key.remoteJid;
+
   const senderNumber = senderJid.split('@')[0].split(':')[0];
+
   return DEV_NUMBERS.includes(senderNumber);
 }
 
