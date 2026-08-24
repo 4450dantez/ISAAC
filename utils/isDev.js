af2f16db6ab8063dd7c22cef54afb7025632cd00
kingplayboi <isaacmaingi067@gmail.com>
2026-08-25 01:50:38 +0300

    Update utils.js

--- utils/isDev.js
+++ utils/isDev.js
@@ -14,39 +14,18 @@ function normalizeNumber(jid) {
     .replace(/\D/g, '');
 }
 
-function isDev(msg, sock) {
+function isDev(msg) {
   if (!msg?.key) return false;
 
-  // 1. If message was sent by the bot instance itself (fromMe)
-  if (msg.key.fromMe) {
-    const botPn = sock?.user?.id
-      ? normalizeNumber(sock.user.id)
-      : '';
+  const senderJid =
+    msg.key.participantPn ||
+    msg.key.participantAlt ||
+    msg.key.participant ||
+    msg.key.remoteJidAlt ||
+    msg.key.remoteJid;
 
-    const botLid = sock?.user?.lid
-      ? normalizeNumber(sock.user.lid)
-      : '';
-
-    if (botPn && DEV_NUMBERS.includes(botPn)) return true;
-    if (botLid && DEV_NUMBERS.includes(botLid)) return true;
-
-    return false;
-  }
-
-  // 2. Candidate JID fields for Group & Direct messages
-  const candidates = [
-    msg.participant,
-    msg.key.participantPn,
-    msg.key.participantAlt,
-    msg.key.participant,
-    msg.key.remoteJidAlt,
-    msg.key.remoteJid
-  ];
-
-  return candidates.some((jid) => {
-    const number = normalizeNumber(jid);
-    return number && DEV_NUMBERS.includes(number);
-  });
+  const number = normalizeNumber(senderJid);
+  return number && DEV_NUMBERS.includes(number);
 }
 
 module.exports = { isDev };
