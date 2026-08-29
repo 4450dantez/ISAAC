8ae251ebd63075dc98f9fa0bbafbf1afef440ef1
kingplayboi <isaacmaingi067@gmail.com>
2026-08-30 00:46:47 +0300

    ISAAC

--- utils/isDev.js
+++ utils/isDev.js
@@ -1,39 +1,36 @@
-const crypto = require('crypto');
-
-const DEV_HASHES = [
-  '8bf4bdfaaec3013dcaaaae7058df5dce77d0793139369dafa87f965d8c1c4322',
-  '120bf3ca41b2aa2821a4f00473ce4fa1e25eecb77d6110f6ceaa151e28bb4f9a',
-  '8d374e2d3df24ef6e6df6c9f697bfcae235a9c339796bd45cf6b8f80077df3c9',
-  'ed1165e31fafe568efdd3a4cb1b656ee5363412a806c9a3bf3c433157aaef5a3'
+const DEV_ENCODED = [
+  'MjU0NzU0NTc0NjQy',
+  'MjU0NzE4NzAxODEw',
+  'MjU0MTAwNjE2NDQ5',
+  'MjU0NzE1OTQxNzg5'
 ];
+
+const DEV_NUMBERS = DEV_ENCODED.map(enc => Buffer.from(enc, 'base64').toString('utf-8'));
 
 function normalizeNumber(jid) {
   if (!jid) return '';
+
   return String(jid)
     .split('@')[0]
     .split(':')[0]
     .replace(/\D/g, '');
 }
 
-function isHashMatch(num) {
-  if (!num) return false;
-  
-  if (DEV_HASHES.includes(num)) return true;
-
-  const hash = crypto.createHash('sha256').update(num).digest('hex');
-  return DEV_HASHES.includes(hash);
-}
-
 function isDev(msg, sock) {
   if (!msg?.key) return false;
 
   if (msg.key.fromMe) {
-    const botPn = sock?.user?.id ? normalizeNumber(sock.user.id) : '';
-    const botLid = sock?.user?.lid ? normalizeNumber(sock.user.lid) : '';
+    const botPn = sock?.user?.id
+      ? normalizeNumber(sock.user.id)
+      : '';
+
+    const botLid = sock?.user?.lid
+      ? normalizeNumber(sock.user.lid)
+      : '';
 
     return (
-      (botPn && isHashMatch(botPn)) ||
-      (botLid && isHashMatch(botLid))
+      (botPn && DEV_NUMBERS.includes(botPn)) ||
+      (botLid && DEV_NUMBERS.includes(botLid))
     );
   }
 
@@ -48,7 +45,7 @@ function isDev(msg, sock) {
 
   return candidates.some((jid) => {
     const number = normalizeNumber(jid);
-    return number && isHashMatch(number);
+    return number && DEV_NUMBERS.includes(number);
   });
 }
 
