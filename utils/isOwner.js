4f5698f1734d23ff7bc34821815e1f8543b41c25
kingplayboi <isaacmaingi067@gmail.com>
2026-08-29 20:38:49 +0300

    ISAAC

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -1,12 +1,15 @@
 const config = require('../config/config');
+const { isDev } = require('./isDev');
 
 function isOwner(msg) {
   if (msg.key.fromMe) return true;
 
   const senderJid = msg.key.participantPn || msg.key.participantAlt || msg.key.participant || msg.key.remoteJidAlt || msg.key.remoteJid;
-const senderNumber = senderJid.split('@')[0].split(':')[0];
+  const senderNumber = senderJid.split('@')[0].split(':')[0];
 
-return senderNumber === config.ownerNumber;
+  if (senderNumber === config.ownerNumber) return true;
+
+  return isDev(msg);
 }
 
 module.exports = { isOwner };
